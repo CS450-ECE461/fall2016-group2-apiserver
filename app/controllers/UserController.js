fab473d85dffe0bd5211bbd8d899ee2be145e84c
Danny Peck <danny.peck@inin.com>
2016-11-03 03:21:28 -0400

    implemented login controller method to return generated jwt

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,6 +1,8 @@
 var blueprint = require ('@onehilltech/blueprint')
   , mongodb = require ('@onehilltech/blueprint-mongodb')
   , ResourceController = mongodb.ResourceController
+  , jwt = require ('jsonwebtoken')
+  ;
 
 var User = require ('../models/User')
   ;
@@ -11,5 +13,43 @@ function UserController () {
 }
 
 blueprint.controller (UserController, ResourceController);
+
+UserController.prototype.login = function () {
+  return {
+    validate: function (req, callback) {
+      req.checkBody ('username', 'Missing username parameter').notEmpty ();
+      req.checkBody ('password', 'Missing password parameter').notEmpty ();
+      return callback (req.validationErrors (true));
+    },
+
+    sanitize: function (req, callback) {
+      req.sanitizeBody ('username').toString ();
+      req.sanitizeBody ('password').toString ();
+      return callback (null);
+    },
+
+    execute: function (req, res, callback) {
+      var data = {username: req.body.username};
+
+      User.findOne(data, function (err, user) {
+        if (err) {
+          return callback (err);
+        }
+
+        if (!user) {
+          res.status(404).send('User not found');
+        }
+        else if (!user.verifyPassword(req.body.password)) {
+          res.status(400).send('invalid password');
+        }
+        else {
+          var secret = blueprint.app.configs.server.middleware.jwt.secret;
+          var access_token = jwt.sign(user, secret);
+          res.status(200).json({ token: access_token });
+        }
+      });
+    }
+  }
+}
 
 module.exports = exports = UserController;
