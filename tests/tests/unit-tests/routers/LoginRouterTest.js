53662fc62956ed622fac71e051883e6d5c76bd03
Danny Peck <danny.peck@inin.com>
2016-11-04 14:51:13 -0400

    added additional test cases for failure cases

--- tests/tests/unit-tests/routers/LoginRouterTest.js
+++ tests/tests/unit-tests/routers/LoginRouterTest.js
@@ -19,34 +19,74 @@ describe ('LoginRouter', function () {
 
     var userData = users[0].user;
     var token;
+    var credentials;
+
+    before (function (done) {
+      var User = blueprint.app.models.User;
+      var newUser = new User (userData);
+
+      newUser.save(function (err, user) {
+        if (err) { return done (err); }
+
+        credentials = {
+          username: user.username,
+          password: user.password
+        }
+
+        return done ();
+      });
+    })
 
     describe ('POST', function () {
       it ('should return valid access_token on login', function (done) {
-        var User = blueprint.app.models.User;
-        var newUser = new User (userData);
+        request (blueprint.app.server.app)
+          .post ('/login')
+          .send (credentials)
+          .expect (200)
+          .end (function (err, res) {
+            if (err) {
+              return done (err);
+            }
 
-        newUser.save(function (err, user) {
-          if (err) { return done (err); }
+            token = res.body.token;
+            expect (token).to.not.be.undefined;
+            return done ();
+          });
+      });
 
-          var credentials = {
-            username: user.username,
-            password: user.password
-          }
+      it ('should fail to login with invalid username', function (done) {
+        var wrongCredentials = {
+          username: 'wrong',
+          password: credentials.password
+        }
 
-          request (blueprint.app.server.app)
-            .post ('/login')
-            .send (credentials)
-            .expect (200)
-            .end (function (err, res) {
-              if (err) {
-                return done (err);
-              }
+        request (blueprint.app.server.app)
+          .post ('/login')
+          .send (wrongCredentials)
+          .expect (404, done);
+      });
 
-              token = res.body.token;
-              expect (token).to.not.be.undefined;
-              return done ();
-            });
-        });
+      it ('should fail to login with invalid password', function (done) {
+        var wrongCredentials = {
+          username: credentials.username,
+          password: 'wrong'
+        }
+
+        request (blueprint.app.server.app)
+          .post ('/login')
+          .send (wrongCredentials)
+          .expect (400, done);
+      });
+
+      it ('should fail to validate credentials on login', function (done) {
+        var wrongCredentials = {
+          username: credentials.username
+        }
+
+        request (blueprint.app.server.app)
+          .post ('/login')
+          .send (wrongCredentials)
+          .expect (400, done);
       });
     });
   });
