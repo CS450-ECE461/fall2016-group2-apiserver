8d7f351b5e6b59daa86554453842302b69e7f1a3
Amanda <amjustin@iupui.edu>
2016-11-08 16:13:37 -0500

    Add organization test data

---
+++ tests/fixtures/organizations.js
@@ -0,0 +1,16 @@
+var organizations = [
+  {
+    name:       'org1',
+    location:   'place1',
+    website:    'org1@org.com',
+    industry:   'tech'
+  },
+  {
+    name:       'org2',
+    location:   'place2',
+    website:    'org2@org.com',
+    industry:   'automotive'
+  }
+]
+
+module.exports = exports = organizations;
