3f357047cb356fe136f1409c8ca1f91d25f53291
Amanda <amjustin@iupui.edu>
2016-11-09 14:45:40 -0500

    eliminated allow field in OrganizationRouter

--- app/routers/v1/OrganizationRouter.js
+++ app/routers/v1/OrganizationRouter.js
@@ -5,9 +5,6 @@ module.exports = exports = {
       controller: 'OrganizationController',
 
       // optional list of operations to allow
-      allow: ['getOne', 'getAll', 'update'],
-
-      // optional list of operations to allow
       deny: ['create', 'delete'],
     }
   }
