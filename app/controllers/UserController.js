1d1bf2b931f9d8d9aa6c6e13d1ed3a0a89f15c51
Nathan White <najowhit@iupui.edu>
2016-11-10 21:00:14 -0500

    function added to query the database to see if a user exists within an org

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -14,4 +14,26 @@ function UserController () {
 
 blueprint.controller (UserController, ResourceController);
 
+UserController.prototype.create = function ()
+{
+  var opts = {
+    on: {
+      preCreate: function (req, doc, callback) {
+        User.findOne ({ username: req.body.username, org_id: req.body.org_id }, function (err, user) {
+          if (err) { return callack (err); }
+
+          if (user) {
+            return callback (new Error ('user already exists'), doc);
+          } else {
+            return callback (null, doc);
+          }
+        });
+      }
+    }
+  };
+
+  return mongodb.ResourceController.prototype.create.call (this, opts);
+};
+
+
 module.exports = exports = UserController;
