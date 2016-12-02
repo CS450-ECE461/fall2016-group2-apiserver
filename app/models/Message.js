629251d881b09e13e3cd39e60f7e7ac4e28be860
Cory <cotz1995@gmail.com>
2016-12-02 18:12:03 -0500

    Issue50: fixed typeo Data --> Date

--- app/models/Message.js
+++ app/models/Message.js
@@ -4,9 +4,9 @@ var mongodb = require ('@onehilltech/blueprint-mongodb')
 var schema = new mongodb.Schema ({
     sender_id:      {type: String, required: true, trim: true},
     receiver_id:    {type: String, required: true, trim: true},
-    received:       {type: Boolean, required: true, trim: true},
-    viewed:         {type: Boolean, required: true, trim: true},
-    timeout:        {type: Data, required: true, trim: true},
+    received:       {type: Boolean, required: true},
+    viewed:         {type: Boolean, required: true},
+    timeout:        {type: Date, required: true},
     title:          {type: String, required: true, trim: true},
     content:        {type: String, required: true, trim: true}
 });
