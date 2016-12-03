0958a2cf2a84594e0ad328a803973e4ca119c87f
Danny Peck <danny.peck@inin.com>
2016-12-03 14:30:06 -0500

    refactored Login and Organization tests to use org_ids

--- tests/tests/unit-tests/routers/OrganizationRouterTest.js
+++ tests/tests/unit-tests/routers/OrganizationRouterTest.js
@@ -3,6 +3,7 @@ var blueprint     = require ('@onehilltech/blueprint')
   , expect        = require ('chai').expect
   , nodemailer    = require ('nodemailer')
   , stubTransport = require ('nodemailer-stub-transport')
+  , async         = require ('async')
   ;
 
 var appPath         = require ('../../../fixtures/appPath');
@@ -26,37 +27,55 @@ describe ('OrganizationRouter', function () {
     var userData;
     var userId;
 
-    var organizationData;
+    var organizationData = organizations[0];
+    var org_id;
     var organizationId;
 
     before (function (done) {
-      adminData = users[1];
-      organizationData = organizations[0];
+      async.series ([
+        function (callback) {
+          var Organization = blueprint.app.models.Organization;
+          var orgData = organizations[0].organization;
 
-      var User = blueprint.app.models.User;
-      var newAdmin = new User (adminData);
+          var organization = new Organization (orgData);
+          organization.save (function (err, res) {
+            if (err) { return callback (err); }
 
-      newAdmin.save (function (err, user) {
-        if (err) { return done (err); }
-
-        var data = {
-          email: user.email,
-          password: user.password
-        }
-
-        request (blueprint.app.server.app)
-          .post ('/admin/login')
-          .send (data)
-          .expect (200)
-          .end (function (err, res) {
-            if (err) {
-              return done (err);
-            }
-
-            adminAccessToken = res.body.token;
-            return done ();
+            org_id = res._id;
+            callback ();
           });
-      });
+        },
+
+        function (callback) {
+          adminData = users[1];
+
+          var User = blueprint.app.models.User;
+          var newAdmin = new User (adminData);
+          newAdmin.org_id = org_id;
+
+          newAdmin.save (function (err, user) {
+            if (err) { return done (err); }
+
+            var data = {
+              email: user.email,
+              password: user.password
+            };
+
+            request (blueprint.app.server.app)
+            .post ('/admin/login')
+            .send (data)
+            .expect (200)
+            .end (function (err, res) {
+              if (err) {
+                return done (err);
+              }
+
+              adminAccessToken = res.body.token;
+              return done ();
+            });
+          });
+        }
+      ])
     });
 
     describe ('Authentication', function () {
@@ -67,6 +86,7 @@ describe ('OrganizationRouter', function () {
         userData = users[0];
         var User = blueprint.app.models.User;
         var newUser = new User (userData);
+        newUser.org_id = org_id;
 
         newUser.save (function (err, user) {
           if (err) { return done (err); }
@@ -111,10 +131,10 @@ describe ('OrganizationRouter', function () {
       var adminId;
 
       it ('should create an organization in the database', function (done) {
+        var orgData = organizations[1];
         request (blueprint.app.server.app)
-          .post ('/v1/admin/organizations') // route
-          .set ('Authorization', 'bearer ' + adminAccessToken)
-          .send (organizationData) // data being sent
+          .post ('/organizations') // route
+          .send (orgData) // data being sent
           .expect (200) // expected statusCode
 
           // end actually sends the request and the callback handles the response
@@ -145,10 +165,11 @@ describe ('OrganizationRouter', function () {
       });
 
       it ('should fail to create an organization with existing name', function (done) {
+        var newOrgData = organizations[1];
+        newOrgData.organization.name = organizations[0].organization.name;
         request (blueprint.app.server.app)
-          .post ('/v1/admin/organizations')
-          .set ('Authorization', 'bearer ' + adminAccessToken)
-          .send (organizationData)
+          .post ('/organizations')
+          .send (newOrgData)
           .expect (400, done);
       });
     });
@@ -183,7 +204,7 @@ describe ('OrganizationRouter', function () {
         updatedOrganization.organization.website = 'webweb@org.com';
 
         request (blueprint.app.server.app)
-          .put ('/v1/admin/organizations/' + organizationId)
+          .put ('/v1/admin/organizations/' + org_id)
           .set ('Authorization', 'bearer ' + adminAccessToken)
           .send (updatedOrganization)
           .expect (200)
@@ -199,7 +220,7 @@ describe ('OrganizationRouter', function () {
     describe ('DELETE', function (done) {
       it ('should delete a single organization in the database', function (done) {
         request (blueprint.app.server.app)
-          .delete ('/v1/admin/organizations/' + organizationId)
+          .delete ('/v1/admin/organizations/' + org_id)
           .set ('Authorization', 'bearer ' + adminAccessToken)
           .expect (200, done);
       });
