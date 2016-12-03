2f030b72b358474e9eb96468b395a4cbc5e0f240
Cory <cotz1995@gmail.com>
2016-12-03 02:01:29 -0500

    Issue54: corrected MessageRouter

--- app/routers/v1/MessageRouter.js
+++ app/routers/v1/MessageRouter.js
@@ -2,14 +2,14 @@
 module.exports = exports = {
     '/messages' : {
         get: { action: 'MessageController@getAll' },
+        post: { action: 'MessageController@create' },
 
-        '/:messageId': {
-            get: { action: 'MessageController@get' },
-            put: { action: 'MessageController@update' },
-            post: { action: 'MessageController@create' }
-        },
         '/received' : {
             get: { action: 'MessageController@getMessagesByReceiver'}
+        },
+        '/:messageId': {
+            get: { action: 'MessageController@get' },
+            put: { action: 'MessageController@update' }
         }
     }
 };
