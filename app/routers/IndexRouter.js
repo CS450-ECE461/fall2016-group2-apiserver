6a1897e42587b50ba4a344164ad36da5e2c7ac96
Danny Peck <danny.peck@inin.com>
2016-12-04 15:42:51 -0500

    removed dev route from release code

--- app/routers/IndexRouter.js
+++ app/routers/IndexRouter.js
@@ -2,11 +2,6 @@ var passport = require ('passport');
 
 module.exports = exports = {
   '/v1': passport.authenticate ('bearer', { session: false }),
-  '/dev': {
-    '/users': {
-      post: { action: 'UserController@create' }
-    }
-  },
   '/organizations': {
     resource: {
       controller: 'OrganizationController'
