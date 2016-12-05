dfef31a5d64cbf05d8dd95fb2339abf3f2a1443a
Danny Peck <danny.peck@inin.com>
2016-12-05 02:36:52 -0500

    added org_id to all incoming messages

--- app/controllers/MessageController.js
+++ app/controllers/MessageController.js
@@ -6,10 +6,34 @@ var blueprint = require ('@onehilltech/blueprint')
     ;
 
 var Message = require ('../models/Message')
+  , User    = require ('../models/User')
+  ;
 
 function MessageController () {
     ResourceController.call (this, {name: 'message', model: Message});
 }
+
+UserController.prototype.create = function ()
+{
+  var opts = {
+    on: {
+      preCreate: function (req, doc, callback) {
+        var token = req.headers.authorization.split(' ')[1];
+
+        User.findOne({token: token}, function (err, user) {
+          if (err) {
+            return callback(err);
+          }
+
+          doc.message.org_id = user.org_id;
+          return callback(null, doc);
+        });
+      }
+    }
+  };
+
+  return mongodb.ResourceController.prototype.create.call (this, opts);
+};
 
 blueprint.controller (MessageController, ResourceController);
 
--- app/models/Message.js
+++ app/models/Message.js
@@ -1,9 +1,13 @@
 var mongodb = require ('@onehilltech/blueprint-mongodb')
     ;
 
+var Organization = require ('./Organization')
+    ;
+
 var schema = new mongodb.Schema ({
     sender_id:      {type: String, required: true, trim: true},
     receiver_id:    {type: String, required: true, trim: true},
+    org_id:         {type: mongodb.Schema.ObjectId, ref: Organization.modelName, required: false},
     received:       {type: Boolean, required: true},
     viewed:         {type: Boolean, required: true},
     timeout:        {type: Date, required: true},
