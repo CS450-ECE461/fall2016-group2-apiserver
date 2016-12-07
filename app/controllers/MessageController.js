b77790991e67e229ecde4f3d3ff22d31f6a4482d
Danny Peck <danieljpeck93@gmail.com>
2016-12-06 22:06:21 -0500

    resolved merge conflicts

--- app/controllers/MessageController.js
+++ app/controllers/MessageController.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var blueprint = require ('@onehilltech/blueprint')
-    , mongodb = require ('@onehilltech/blueprint-mongodb')
-    , ResourceController = mongodb.ResourceController
-    , async = require('async')
-    ;
+  , mongodb = require ('@onehilltech/blueprint-mongodb')
+  , ResourceController = mongodb.ResourceController
+  , async = require ('async')
+  ;
 
 var Message = require ('../models/Message')
   , User    = require ('../models/User')
   ;
 
 function MessageController () {
-    ResourceController.call (this, {name: 'message', model: Message});
+  ResourceController.call (this, {name: 'message', model: Message});
 }
 
 MessageController.prototype.create = function () {
@@ -61,7 +61,28 @@ MessageController.prototype.getMessagesByOrg = function () {
   }
 };
 
-blueprint.controller (MessageController, ResourceController);
+MessageController.prototype.getMessagesBySender = function () {
+  return function (req, res) {
+    var token = req.headers.authorization.split(' ')[1];
+
+    async.waterfall ([
+      function (callback) {
+        User.findOne ({token: token}, callback)
+      },
+      function (user, callback) {
+        Message.find ({sender_email: user.email}, {__v: 0}, callback);
+      },
+      function (messages, callback) {
+        res.status (200).json ({messages: messages});
+        return callback (null);
+      }
+    ],
+    /* istanbul ignore next */
+    function (err) {
+      if (err) { res.status (400).send (err); }
+    });
+  }
+};
 
 MessageController.prototype.getMessagesByReceiver = function () {
     return function(req, res){
@@ -90,5 +111,6 @@ MessageController.prototype.getMessagesByReceiver = function () {
     }
 };
 
+blueprint.controller (MessageController, ResourceController);
 
 module.exports = MessageController;
