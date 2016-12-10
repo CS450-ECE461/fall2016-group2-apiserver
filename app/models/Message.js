abfd6e88ad074aff54da4ab72becd5092aeb38f8
Danny Peck <danieljpeck93@gmail.com>
2016-12-09 23:38:49 -0500

    updated the model

--- app/models/Message.js
+++ app/models/Message.js
@@ -5,14 +5,14 @@ var Organization = require ('./Organization')
   ;
 
 var schema = new mongodb.Schema ({
-  sender_email:   {type: String, required: true, trim: true},
-  receiver_email: {type: String, required: true, trim: true},
-  org_id:         {type: String, required: false, trim: true},
-  received:       {type: Boolean, required: true},
-  viewed:         {type: Boolean, required: true},
-  expireAt:       {type: Date, required: false, default: Date.now()},
-  title:          {type: String, required: true, trim: true},
-  content:        {type: String, required: true, trim: true}
+  sender:   {type: String, required: true, trim: true},
+  receiver: {type: String, required: true, trim: true},
+  org_id:   {type: String, required: false, trim: true},
+  received: {type: Boolean, required: true},
+  viewed:   {type: Boolean, required: true},
+  expireAt: {type: Date, required: false, default: Date.now()},
+  title:    {type: String, required: true, trim: true},
+  content:  {type: String, required: true, trim: true}
 });
 
 const COLLECTION_NAME = 'Message';
