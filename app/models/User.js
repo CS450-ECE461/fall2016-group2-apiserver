afbf5ee784ec657aeb3a9fa4813c3cc8bf1178d5
Danny Peck <danieljpeck93@gmail.com>
2016-12-15 22:35:01 -0500

    refactored models to increase code quality

--- app/models/User.js
+++ app/models/User.js
@@ -9,18 +9,14 @@ var schema = new mongodb.Schema({
   password:   {type: String, required: true, trim: true},
   job_title:  {type: String, required: true, trim: true},
   role:       {type: String, required: true, enum: roles},
-  org_id:     {type: String, required: false, trim: true}, // not required until orgs exist
-  token:      {type: String, required: false, trim: true},
+  org_id:     {type: String, required: false, trim: true},
+  token:      {type: String, required: false, trim: true}
 });
 
 const COLLECTION_NAME = 'user';
 
 schema.methods.verifyPassword = function (password) {
-  if (this.password == password) {
-    return true;
-  }
-
-  return false;
+  return this.password === password;
 };
 
 module.exports = exports = mongodb.model (COLLECTION_NAME, schema);
