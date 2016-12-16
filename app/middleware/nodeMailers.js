157c05d57a41f46ee531882020ba6cfc21bfe35c
Danny Peck <danieljpeck93@gmail.com>
2016-12-15 22:44:11 -0500

    refactored nodeMailers to further reduce code duplication

--- app/middleware/nodeMailers.js
+++ app/middleware/nodeMailers.js
@@ -3,6 +3,27 @@ var stubTransport = require('nodemailer-stub-transport');
 
 var mailers = {};
 
+var sendEmail = function (transporter, emailer, docs) {
+  var message = '<p>Thank you ' + docs.organization.name + ' for joining Hive!</p>';
+  message += '<p>Your admin credentials are listed below!</p>';
+  message += '<p>username: ' + docs.user.username + '</p>';
+  message += '<p>password: ' + docs.user.password + '</p>';
+
+  var mailOptions = {
+    from: '"HiveEmailer" <' + emailer + '>',
+    to: '"' + docs.user.username + '" <' + docs.user.email + '>',
+    subject: 'Welcome to Hive',
+    text: 'Here is your admin username and password:',
+    html: message
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(err, info){
+    if (err) { return callback (err); }
+    callback (null, {org_id: docs.organization._id, admin_id: docs.user._id});
+  });
+};
+
 /* istanbul ignore next */
 mailers.mailerTransport  = function (docs, callback) {
 
@@ -22,49 +43,16 @@ mailers.mailerTransport  = function (docs, callback) {
   // create reusable transporter object using the default SMTP transport
   var transporter = nodemailer.createTransport(connection);
 
-  var message = '<p>Thank you ' + docs.organization.name + ' for joining Hive!</p>';
-  message += '<p>Your admin credentials are listed below!</p>';
-  message += '<p>username: ' + docs.user.username + '</p>';
-  message += '<p>password: ' + docs.user.password + '</p>';
+  sendEmail (transporter, emailer, docs);
+};
 
-  var mailOptions = {
-    from: '"HiveEmailer" <' + emailer + '>',
-    to: '"' + docs.user.username + '" <' + docs.user.email + '>',
-    subject: 'Welcome to Hive',
-    text: 'Here is your admin username and password:',
-    html: message
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(err, info){
-    if (err) { return callback (err); }
-    callback (null, {org_id: docs.organization._id, admin_id: docs.user._id});
-  });
-}
-
-mailers.mailerStub  = function (docs, callback) {
-  var transport = nodemailer.createTransport(stubTransport());
+mailers.mailerStub = function (docs, callback) {
+  var transporter = nodemailer.createTransport(stubTransport());
 
   var emailer = 'HiveEmailer@gmail.com';
 
-  var message = '<p>Thank you ' + docs.organization.name + ' for joining Hive!</p>';
-  message += '<p>Your admin credentials are listed below!</p>';
-  message += '<p>username: ' + docs.user.username + '</p>';
-  message += '<p>password: ' + docs.user.password + '</p>';
-
-  var mailOptions = {
-    from: '"HiveEmailer" <' + emailer + '>',
-    to: '"' + docs.user.username + '" <' + docs.user.email + '>',
-    subject: 'Welcome to Hive',
-    text: 'Here is your admin username and password:',
-    html: message
-  };
-
-  transport.sendMail (mailOptions, function (error, info){
-    if (error) { return callback (error); }
-    callback (null, {org_id: docs.organization._id, admin_id: docs.user._id});
-  });
-}
+  sendEmail (transporter, emailer, docs);
+};
 
 mailers.resolveMailer = function () {
   if (process.env.NODE_ENV === 'test') {
@@ -74,6 +62,6 @@ mailers.resolveMailer = function () {
     /* istanbul ignore next */
     return mailers.mailerTransport;
   }
-}
+};
 
 module.exports = exports = mailers;
